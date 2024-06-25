e1098494ca3a5522a0039539c92e9b3b8ee27c7b
SHETAKA12 <kaleagle12@gmail.com>
2024-06-25 19:54:10 +0300

    created a function to get the result but not working

--- app.js
+++ app.js
@@ -4,22 +4,60 @@ const displayResult = document.getElementById("result");
 const choices = document.querySelectorAll("button");
 
 let userChoice
-
+let computerChoice
+let result
 choices.forEach(choice => choice.addEventListener('click', (e) => {
     userChoice = e.target.id
     displayUser.textContent = userChoice
     generateComputerChoice()
+    console.log(result)
+    getResult()
 }))
 
 function generateComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3)
-    if (computerChoice === 0) {
+    const randomNumber = Math.floor(Math.random() * 3)
+    
+    if (randomNumber === 0) {
         computerChoice = "rock"
-    } else if (computerChoice === 1) {
+    } else if (randomNumber === 1) {
         computerChoice = "paper"
     } else {
         computerChoice = "scissors"
     }
     displayComputer.textContent = computerChoice
+   
     
 }
+
+function getResult() {
+    if (computerChoice === userChoice){
+        result = "It's a tie!"
+    }
+
+    if (computerChoice === "Rock" && userChoice === "Paper"){
+        result = "You win!"
+    }
+
+    if (computerChoice === "Rock" && userChoice === "Scissors"){
+        result = "You lose!"
+    }
+
+    if (computerChoice === "Paper" && userChoice === "Rock"){
+        result = "You lose!"
+    }
+
+    if (computerChoice === "Paper" && userChoice === "Scissors"){
+        result = "You win!"
+    }
+
+    if (computerChoice === "Scissors" && userChoice === "Rock"){
+        result = "You win!"
+    }
+
+    if (computerChoice === "Scissors" && userChoice === "Paper"){
+        result = "You lose!"
+    }
+    displayResult.innerHTML = result
+
+}
+
